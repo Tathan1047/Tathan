59611a1c750bfe72d655194a3994c5262e86e352
Jonathan Martinez <jmmdaya@gmail.com>
2018-08-02 15:47:30 -0500

    Ready models student, city, documenttype

--- school/apps/student/admin.py
+++ school/apps/student/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
 
-from apps.student.models import Student, Documenttype
+from apps.student.models import Student, Documenttype, City
 
 
 class AdminStudent(admin.ModelAdmin):
-    list_display = ['code_student', 'name_student', 'lastname_student'
-                    'document_type',
-                    ]
+    list_display = ['code_student', 'name_student', 'lastname_student',
+                    'document_type','number_document','gender', 'birthday','city',
+                    'address','neighborhood','number_telephone','cellphone_number',
+                    'register_date']
 
 
 class AdminDocumenttype(admin.ModelAdmin):
-    list_display = []
+    list_display = ['documet_type']
 
 
+class AdminCity(admin.ModelAdmin):
+    list_display = ['city']
 
 
+admin.site.register(Student, AdminStudent)
+admin.site.register(Documenttype, AdminDocumenttype)
+admin.site.register(City, AdminCity)
--- school/apps/student/models.py
+++ school/apps/student/models.py
@@ -7,6 +7,12 @@ class Documenttype(models.Model):
     def __str__(self):
         return self.documet_type
 
+class City (models.Model):
+    city=models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.city
+
 
 class Student (models.Model):
     code_student= models.CharField(max_length=10, primary_key= True)
@@ -16,9 +22,9 @@ class Student (models.Model):
     number_document=models.CharField(max_length=30)
     gender=models.CharField(choices=(("Masculino","Masculino"),("Femenino","Femenino")),max_length=30)
     birthday=models.DateField
-    city=models.CharField(max_length=30)
+    city= models.ForeignKey(City, on_delete=models.CASCADE)
     address=models.CharField(max_length=60)
     neighborhood=models.CharField(max_length=50)
     number_telephone=models.CharField(max_length=40)
     cellphone_number=models.CharField(max_length=40)
-    register_date=models.DateField
+    register_date=models.DateField()
